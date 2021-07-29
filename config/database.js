e8be8e1e3d1fc1aaaab21c84a2992fce2bd3427a
José Padron <padroncjosel@gmail.com>
2021-07-29 18:00:18 -0400

    sqlite local

--- config/database.js
+++ config/database.js
@@ -4,14 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'api-joselpadronc'),
-        username: env('DATABASE_USERNAME', 'joselpadronc'),
-        password: env('DATABASE_PASSWORD', '176605**'),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      options: {},
+      options: {
+        useNullAsDefault: true,
+      },
     },
   },
 });
